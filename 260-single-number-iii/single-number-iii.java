da2d42f3f26bd3b678446f2e241d063aa31a7193
abhishek ruikar <abhishekruikar7@gmail.com>
2026-07-25 18:45:27 +0530

    Time: 8 ms (15.20%) | Memory: 47.5 MB (97.06%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -5,16 +5,14 @@ class Solution {
             mp.put(i,mp.getOrDefault(i,0)+1);
 
         }
-        ArrayList<Integer> ans = new ArrayList<>();
+        int[] ans = new int[2];
+        int a = 0;
         for(int i = 0;i < nums.length;i++){
             if(mp.get(nums[i]) == 1){
-                ans.add(nums[i]);
+                ans[a] = nums[i];
+                a++;
             }
         }
-        int[] res = new int[ans.size()];
-        for(int i = 0;i < ans.size();i++){
-            res[i] = ans.get(i);
-        }
-        return res;
+        return ans;
     }
 }
