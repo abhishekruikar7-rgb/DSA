732fabb402b56be905d32b5dd07058b95d5cb006
abhishek ruikar <abhishekruikar7@gmail.com>
2026-07-25 17:22:07 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (9.35%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i : nums){
+            mp[i]++;
+        }
+        vector<int> ans;
+        for(int i = 0;i < nums.size();i++){
+            if(mp[nums[i]] == 1){
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
