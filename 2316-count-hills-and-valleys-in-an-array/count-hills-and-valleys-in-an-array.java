2775f35e0210dec00141777303c34c1836d8651d
abhishek ruikar <abhishekruikar7@gmail.com>
2026-08-15 23:20:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (73.38%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+        int c = 0;
+        int left = 0;
+        for(int i = 1;i < nums.length-1;i++){
+            if(nums[i] != nums[i+1]){
+                if((nums[i] > nums[left] && nums[i] > nums[i+1]) || (nums[i] < nums[left] && nums[i] < nums[i+1])){
+                    c++;
+                }
+                left = i;
+            }
+        }
+        return c;
+    }
+}
